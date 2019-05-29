bf7b7c02b2bbdd64b933de1302f8714749d58d89
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 03:13:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,6 +17,9 @@ def word_substituter(s)
   s
 end
 
+a = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+b = 
+
 word_substituter 
 
 def bulk_tweet_shortener(array)
