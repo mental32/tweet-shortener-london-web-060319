4ecb4a2a479a7648165fb9facbc5fce2408f454c
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 03:18:19 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,12 +17,6 @@ def word_substituter(s)
   s
 end
 
-a = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
-b = "Hey guys, can anyone teach me how 2 b cool? I really want 2 b the best @ everything, u know what I mean? Tweeting is super fun u guys!!!!"
-
-pp word_substituter a
-pp a == b
-
 def bulk_tweet_shortener(array)
   array.each { |s| puts word_substituter s }
 end
