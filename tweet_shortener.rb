bc2f62b7b8f39154698e60e8ab7b285ac2d5b4cb
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 02:49:11 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,21 @@
+```
+"hello" becomes 'hi'
+"to, two, too" become '2' 
+"for, four" become '4'
+'be' becomes 'b'
+'you' becomes 'u'
+"at" becomes "@" 
+"and" becomes "&"
+```
+
 words = {
-  
+  ['hello'] => 'hi',
+  ['to', 'two', 'too'] => '2',
+  ['for', 'four'] => '4'
+  ['be'] => 'b',
+  ['you'] => 'u',
+  ['at'] => '@',
+  ['and'] => '&'
 }
 
 def word_substituter(s)
