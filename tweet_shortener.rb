c2047ea5f2ff93caf18c2daf0ca8e42e9772bbdc
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 02:49:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,13 +1,3 @@
-```
-"hello" becomes 'hi'
-"to, two, too" become '2' 
-"for, four" become '4'
-'be' becomes 'b'
-'you' becomes 'u'
-"at" becomes "@" 
-"and" becomes "&"
-```
-
 words = {
   ['hello'] => 'hi',
   ['to', 'two', 'too'] => '2',
