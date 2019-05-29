da142961c36fc6d334989a659c1a3659486cca00
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 03:13:37 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -20,7 +20,8 @@ end
 a = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
 b = "Hey guys, can anyone teach me how 2 b cool? I really want 2 b the best @ everything, u know what I mean? Tweeting is super fun u guys!!!!"
 
-word_substituter a
+pp word_substituter a
+pp b
 
 def bulk_tweet_shortener(array)
   array.each { |s| puts word_substituter s }
