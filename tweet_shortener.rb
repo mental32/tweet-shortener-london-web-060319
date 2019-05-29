c129859fb06d106c9f69897e6bfe2fa5b23cb6bf
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 03:18:58 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,7 +18,7 @@ def word_substituter(s)
 end
 
 def bulk_tweet_shortener(array)
-  array.each { |s| puts word_substituter s }
+  array.each { |s| puts word_substituter(s) }
 end
 
 # selective_tweet_shortener
