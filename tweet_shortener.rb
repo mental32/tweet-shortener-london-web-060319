5f53ce2944b9204c39093fcec8efe6dac61b7c79
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 03:03:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -8,9 +8,11 @@ words = {
   ['and'] => '&'
 }
 
-words = Hash[ *words.keys.flatten.collect { |k| [ words.keys.select { |a| a.include? k } ] } ]
+words = Hash[ *words.keys.flatten.collect { |k| [ k, words.keys.select { |a| a.include? k } ] } ]
 
 require 'pp'
+
+pp words
 
 def word_substituter(s)
 
